81932b846d98d4fc64a46bdf68c5a397ec551084
Richmond Ko <richmond.ko@rebtel.com>
2023-06-05 13:09:44 +0200

    Initial Commit

---
+++ ios architecture demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios architecture demo
+//
+//  Created by Richmond Ko on 2023-06-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
